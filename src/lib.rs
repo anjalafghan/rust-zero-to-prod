296ceeef770333d8abb4dfb508df63712d29351b
Anjal Afghan <anjal.afghan93@gmail.com>
2023-11-15 20:38:04 +0530

    working tests for form inputs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
-// We need to mark `run` as public.
-// It is no longer a binary entrypoint, therefore we can mark it as async // without having to use any proc-macro incantation.
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run();
+
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use newsletter::run;
+use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("error setting up listener");
+    run(listener)?.await
 }
