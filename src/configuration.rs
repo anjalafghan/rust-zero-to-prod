b7bfafbca3c5a3e6b0a55da0ab1b7bc8e2bf7ed7
Anjal Afghan <anjal.afghan93@gmail.com>
2023-11-18 17:49:25 +0530

    Chapter 3 complete

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,16 +3,6 @@ pub struct Settings {
     pub database: DatabaseSettings,
     pub application_port: u16,
 }
-pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    // Initialise our configuration reader
-    let mut settings = config::Config::default();
-    // Add configuration values from a file named `configuration`. // It will look for any top-level file with an extension
-    // that `config` knows how to parse: yaml, json, etc.
-    settings.merge(config::File::with_name("configuration"))?;
-    // Try to convert the configuration values it read into
-    // our Settings type
-    settings.try_into()
-}
 
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
@@ -22,6 +12,7 @@ pub struct DatabaseSettings {
     pub host: String,
     pub database_name: String,
 }
+
 impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!(
@@ -29,4 +20,21 @@ impl DatabaseSettings {
             self.username, self.password, self.host, self.port, self.database_name
         )
     }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+    settings.try_deserialize::<Settings>()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
-use sqlx::{Connection, PgConnection};
+use sqlx::{Connection, PgConnection, PgPool};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Error getting config");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    let connection = PgConnection::connect(&configuration.database.connection_string())
-        .await
-        .expect("Error connecting to postgres");
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
